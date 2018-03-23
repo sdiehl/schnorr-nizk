02f2e1ee2803057b6b60f633c1cc0ef5eb31eff5
Alberto Centelles <centelles.alberto@gmail.com>
2018-03-23 16:18:05 +0000

    Add QuickCheck tests for interactive variant

--- test/Main.hs
+++ test/Main.hs
@@ -3,18 +3,22 @@
 module Main where
 
 import           Data.ByteString
-import           NonInteractive
-import           Schnorr
+import           Protolude
+import           Test.QuickCheck.Monadic
 import           Test.Tasty
 import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck
+
+import           NonInteractive
+import           Schnorr
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+tests = testGroup "Tests" [schnorrTests]
 
-unitTests = testGroup "Schnorr Indentification Schemes"
+schnorrTests = testGroup "Schnorr Indentification Schemes"
   [ testCaseSteps "Non-interactive" $ \step -> do
       step "Alice generates private and public keys..."
       (pubKey, privKey) <- generateKeys
@@ -31,20 +35,14 @@ unitTests = testGroup "Schnorr Indentification Schemes"
       step "Bob only verifies that Alice knows the value of the private key..."
       assertBool "Non-Interactive Schnorr doesn't work" $ verify pubKey pubCommit challenge r
 
-  -- , testCaseSteps "Interactive" $ \step -> do
-  --     step "Alice generates private and public keys..."
-  --     (pubKey, privKey) <- generateKeys
-  --
-  --     step "Alice also generates private and public commitment values..."
-  --     (pubCommit, privCommit) <- generateCommit
-  --
-  --     TODO: Mock IO to enter a random message
-  --     step "Bob generates a challenge..."
-  --     challenge <- generateChallenge msg
-  --
-  --     step "Alice computes the response..."
-  --     let r = computeResponse privCommit privKey challenge
-  --
-  --     step "Bob verifies that Alice knows the value of the private key..."
-  --     assertBool "Interactive Schnorr doesn't work" $ verify pubKey pubCommit challenge r
+
+  , testProperty "Interactive" interactiveTest
   ]
+
+interactiveTest :: [Char] -> Property
+interactiveTest msg = monadicIO $ do
+    (pubKey, privKey) <- liftIO generateKeys
+    (pubCommit, privCommit) <- liftIO generateCommit
+    challenge <- liftIO $ generateChallenge (show msg)
+    let r = computeResponse privCommit privKey challenge
+    pure $ verify pubKey pubCommit challenge r
