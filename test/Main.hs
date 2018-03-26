050256096bb9cd601a63aa98834e0253d7fa258f
Alberto Centelles <centelles.alberto@gmail.com>
2018-03-26 15:00:48 +0100

    Add soundness property to tests

--- test/Main.hs
+++ test/Main.hs
@@ -8,10 +8,12 @@ import           Test.QuickCheck.Monadic
 import           Test.Tasty
 import           Test.Tasty.HUnit
 import           Test.Tasty.QuickCheck
+import           Crypto.Number.Generate     (generateBetween)
 
 import           NonInteractive
 import           Interactive
 import           Schnorr
+import           Curve
 
 main :: IO ()
 main = defaultMain tests
@@ -20,7 +22,7 @@ tests :: TestTree
 tests = testGroup "Tests" [schnorrTests]
 
 schnorrTests = testGroup "Schnorr Indentification Schemes"
-  [ testCaseSteps "Non-interactive" $ \step -> do
+  [ testCaseSteps "Non-interactive. Completeness property" $ \step -> do
       step "Alice generates private and public keys..."
       (pubKey, privKey) <- generateKeys
 
@@ -31,14 +33,32 @@ schnorrTests = testGroup "Schnorr Indentification Schemes"
       let challenge = mkChallenge pubKey pubCommit
 
       step "Alice computes the response..."
-      let r = computeResponse privCommit privKey challenge
+      let resp = computeResponse privCommit privKey challenge
 
       step "Bob only verifies that Alice knows the value of the private key..."
-      assertBool "Non-Interactive Schnorr doesn't work" $ verify pubKey pubCommit challenge r
+      assertBool "Non-Interactive Schnorr doesn't work" $ verify pubKey pubCommit challenge resp
+
+  , testCaseSteps "Non-interactive. Soundness property" $ \step -> do
+      step "Alice generates private and public keys..."
+      (pubKey, privKey) <- generateKeys
+
+      step "Alice also generates private and public commitment values..."
+      (pubCommit, privCommit) <- generateCommitment
+
+      step "Using a secure cryptographic hash function to issue the challenge instead..."
+      let challenge = mkChallenge pubKey pubCommit
+
+      step "Alice computes the response but doesn't know the random commitment private value..."
+      randomPrivCommit <- generateBetween 1 (n - 1)
+      let resp = computeResponse randomPrivCommit privKey challenge
+
+      step "Bob only verifies that Alice knows the value of the private key..."
+      assertBool "Non-Interactive Schnorr doesn't work" $ not $ verify pubKey pubCommit challenge resp
 
 
   , testProperty "Interactive" interactiveTest
   ]
+
 
 interactiveTest :: [Char] -> Property
 interactiveTest msg = monadicIO $ do
